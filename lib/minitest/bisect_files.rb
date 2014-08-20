3302b89b0335e5a4a1d22caf7d01044a3d0eef3b
Ryan Davis <ryand@zenspider.com>
2014-08-20 15:43:36 -0800

    Cleaned up by pre-declaring minitest mod [git-p4: depot-paths = "//src/minitest-bisect/dev/": change = 9675]

--- lib/minitest/bisect_files.rb
+++ lib/minitest/bisect_files.rb
@@ -1,51 +1,51 @@
 require "minitest/find_minimal_combination"
 require "shellwords"
 
-module Minitest
-  class BisectFiles
-    def self.run files
-      new.run files
+module Minitest; end
+
+class Minitest::BisectFiles
+  def self.run files
+    new.run files
+  end
+
+  def build_cmd cmd, culprits, bad, rb_flags, mt_flags
+    return false if bad and culprits.empty?
+
+    tests = (culprits + [bad]).flatten.compact.map {|f| %(require "./#{f}")}.join " ; "
+
+    cmd   = %(ruby #{rb_flags.shelljoin} -e '#{tests}' -- #{mt_flags.shelljoin} &> /dev/null)
+
+    cmd
+  end
+
+  def run files
+    files, flags = files.partition { |arg| File.file? arg }
+    rb_flags, mt_flags = flags.partition { |arg| arg =~ /^-I/ }
+
+    cmd = build_cmd(nil, files, nil, rb_flags, mt_flags)
+
+    puts "reproducing..."
+
+    if system cmd then
+      puts "passed??"
+      exit 1
     end
 
-    def build_cmd cmd, culprits, bad, rb_flags, mt_flags
-      return false if bad and culprits.empty?
+    puts "reproduced"
 
-      tests = (culprits + [bad]).flatten.compact.map {|f| %(require "./#{f}")}.join " ; "
+    count = 0
 
-      cmd   = %(ruby #{rb_flags.shelljoin} -e '#{tests}' -- #{mt_flags.shelljoin} &> /dev/null)
+    found = files.find_minimal_combination do |test|
+      count += 1
 
-      cmd
+      puts "# of culprits: #{test.flatten.compact.size}"
+
+      not system build_cmd(nil, test, nil, rb_flags, mt_flags)
     end
 
-    def run files
-      files, flags = files.partition { |arg| File.file? arg }
-      rb_flags, mt_flags = flags.partition { |arg| arg =~ /^-I/ }
-
-      cmd = build_cmd(nil, files, nil, rb_flags, mt_flags)
-
-      puts "reproducing..."
-
-      if system cmd then
-        puts "passed??"
-        exit 1
-      end
-
-      puts "reproduced"
-
-      count = 0
-
-      found = files.find_minimal_combination do |test|
-        count += 1
-
-        puts "# of culprits: #{test.flatten.compact.size}"
-
-        not system build_cmd(nil, test, nil, rb_flags, mt_flags)
-      end
-
-      puts
-      puts "Final found in #{count} steps:"
-      puts
-      puts build_cmd nil, found, nil, rb_flags, mt_flags
-    end
+    puts
+    puts "Final found in #{count} steps:"
+    puts
+    puts build_cmd nil, found, nil, rb_flags, mt_flags
   end
 end
