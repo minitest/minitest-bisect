157f94c0b19fe766ba1b4cd1405cf9460f01c91a
Ryan Davis <ryand@zenspider.com>
2016-06-15 14:00:03 -0800

    prepped for release [git-p4: depot-paths = "//src/minitest-bisect/dev/": change = 10773]

--- lib/minitest/bisect.rb
+++ lib/minitest/bisect.rb
@@ -5,7 +5,7 @@ require "rbconfig"
 require "path_expander"
 
 class Minitest::Bisect
-  VERSION = "1.3.1"
+  VERSION = "1.4.0"
 
   class PathExpander < ::PathExpander
     TEST_GLOB = "**/{test_*,*_test,spec_*,*_spec}.rb" # :nodoc:
